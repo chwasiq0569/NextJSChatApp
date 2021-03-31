c86480998fadba325b7d9d99502c86617091bcb2
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-03-31 23:51:53 +0500

    Created Loading COmponent

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import {auth} from "../firebase"
 import { useAuthState } from "react-firebase-hooks/auth"
 import Login from './components/Login';
+import Loading from "./components/Loading"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth)
+  const [user, loading] = useAuthState(auth)
+
+  if(loading) return <Loading />
 
   if(!user) return <Login />
 
---
+++ pages/components/Loading.js
@@ -0,0 +1,12 @@
+import { Circle } from "better-react-spinkit"
+
+const Login = () => {
+
+    return (
+        <center style={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <Circle color="rgba(133, 203, 248, 1)" size={60} />
+        </center>
+    );
+}
+ 
+export default Login;
